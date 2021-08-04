49a64989e396c86e757ea405d7f88835134be316
yqni13 <65848806+yqni13@users.noreply.github.com>
2021-08-04 11:38:59 +0200

    updated functionality

--- client/js/main.js
+++ client/js/main.js
@@ -1,32 +1,53 @@
-let currentTime;
 
-let divTime = document.getElementById("clock_time");
+function updateTime() {
+    
+    let currentDate = new Date();       //get timestamp
+    
+    let hour = document.getElementById("hour");
+    let minute = document.getElementById("minute");
+    let second = document.getElementById("second");
 
+    let hourValue = currentDate.getHours();
+    let minuteValue = currentDate.getMinutes();
+    let secondValue = currentDate.getSeconds();
 
+    if (hourValue >= 0 && hourValue <= 9) { hourValue = "0" + hourValue; };
+    if (minuteValue >= 0 && minuteValue <= 9) { minuteValue = "0" + minuteValue; };
+    if (secondValue >= 0 && secondValue <= 9) { secondValue = "0" + secondValue; };
 
-let currentDate = new Date();
-let currentDayName = currentDate.getDay();
-let currentDay = currentDate.getDate();
-let currentMonth = currentDate.getMonth()+1;
-let currentYear = currentDate.getFullYear();
+    hour.innerHTML = hourValue;
+    minute.innerHTML = minuteValue;
+    second.innerHTML = secondValue;
 
-let weekdays = [
-    'monday',
-    'tuesday',
-    'wednesday',
-    'thursday',
-    'friday',
-    'saturday',
-    'sunday'
-];
+    let currentDayName = currentDate.getDay();
+    let currentDay = currentDate.getDate();
+    if (currentDay >= 0 && currentDay <= 9) { currentDay = "0" + currentDay; };
+    let currentMonth = currentDate.getMonth()+1;
+    if (currentMonth >= 0 && currentMonth <= 9) { currentMonth = "0" + currentMonth; };
+    let currentYear = currentDate.getFullYear();
+    
 
-for (let i = 0; i <= currentDayName; ++i) {
-    if (i == currentDayName) {
-        currentDayName = weekdays[i-1];
+    let weekdays = [
+        'Montag',
+        'Dienstag',
+        'Mittwoch',
+        'Donnerstag',
+        'Freitag',
+        'Samstag',
+        'Sonntag'
+    ];
+
+    for (let i = 0; i <= currentDayName; ++i) {
+        if (i == currentDayName) {
+            currentDayName = weekdays[i-1];
+        }
     }
+
+    let divDate = document.getElementById("clock_date");
+    divDate.innerHTML = currentDayName + ", " + currentDay + "." + currentMonth + "." + currentYear;
+
 }
 
-let divDate = document.getElementById("clock_date");
-divDate.innerHTML = currentDayName + ", " + currentDay + "." + currentMonth + "." + currentYear;
-
-
+window.onload = function () {
+    setInterval(updateTime, 1000);
+}
